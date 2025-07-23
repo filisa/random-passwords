901fdcebf72a6e4563be66de3c75990dfff522f6
Margarita Nikulina <ritan@fr24.com>
2025-07-23 14:19:08 +0200

    changed everything in scrimba and reuploaded
    changed everything in scrimba, let's see if it works on a real browser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let passwordOne = document.getElementById("rectangleOne")
 let passwordTwo = document.getElementById("rectangleTwo")
-
+const buttonEl = document.getElementById("generatePasswords")
+const copyRandomPasswordOne = document.getElementById("copyRandomPasswordOne")
+const copyRandomPasswordTwo = document.getElementById("copyRandomPasswordTwo")
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-function generatePasswords() {
+buttonEl.addEventListener("click", function() {
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
     let passwordLength = 15
     for (let i = 0; i < passwordLength; i++) {
         let randomNumberOne = Math.floor (Math.random() * characters.length)//0-0,999
@@ -12,6 +16,19 @@ function generatePasswords() {
         passwordOne.textContent += characters[randomNumberOne]
         passwordTwo.textContent += characters[randomNumberTwo]
     }
-}
+} )
+
+copyRandomPasswordOne.addEventListener("click", function() {
+    navigator.clipboard.writeText(passwordOne.textContent);
+    // Alert the copied text
+    alert("Copied the text: " + passwordOne.textContent);
+})
+
+copyRandomPasswordTwo.addEventListener("click", function() {
+    navigator.clipboard.writeText(passwordTwo.textContent);
+    // Alert the copied text
+    alert("Copied the text: " + passwordTwo.textContent);
+})
+
 
 
